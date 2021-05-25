73ed71c9b99e6bc9eeb8679c2248ce202bd834ba
qinglei.zhang <zm7443582@163.com>
2021-05-25 23:10:07 +0800

    main
    rukou wenjian

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @Author: zhangqinglei
+ * @Date:   2021-05-25 23:09:37
+ * @Last Modified by:   zhangqinglei
+ * @Last Modified time: 2021-05-25 23:09:54
+ * @note: 
+ */
+phpinfo();
